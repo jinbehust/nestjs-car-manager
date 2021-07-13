aace6be68437662b6fef758d3e1b720d2c3130f5
Giang Nguyen Duc <ducgiangbkhn@gmail.com>
2021-07-13 14:55:44 +0700

    cache env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({ cache: true })],
   controllers: [AppController],
   providers: [AppService],
 })
